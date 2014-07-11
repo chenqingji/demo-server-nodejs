8ff505b0387fe6e3de615e123efdcbab14f217a7
王小冋 <heroicstirp@163.com>
2014-07-11 18:31:03 +0800

    add comments

--- src/server.js
+++ src/server.js
@@ -6,16 +6,23 @@
  * Time: 18:01
  */ 
 
-var host = '0.0.0.0';
-var port = 8080;
+var host = '0.0.0.0';//demoServer服务器监听的ip地址，0.0.0.0表示监听所有网卡
+var port = 8080; //demoServerb服务器监听的端口号
 var http = require('http');
+
+//解析基本的文件夹未知
 var qs = require('querystring');
 var path = require('path');
 var basedir = path.resolve(__dirname,"..");
 var source = path.resolve(__dirname);
+
+//读取路由信息
 var routes = require(path.join(source,'route.json'));
 
+//读取配置信息
 var conf = require(path.join(source,'conf.json'));
+
+//初始化数据库，用于模拟开发者的数据库信息
 var sqlite3 = require('sqlite3').verbose();
 var db;
 if (conf.sqliteMem){
@@ -27,12 +34,15 @@ db.serialize(function() {
     db.run(conf.dbcreatsql);
 });
 
+//创建服务器
 http.createServer(function (req, res) { 
  	req.finish = false;
 
     for(var route in routes){
         if (routes[route].path == req.url && routes[route].method == req.method){
             var processor = require(path.join(source,"processors", req.url));
+
+            //设置30秒超时返回值
  			setTimeout(function(){
  				if (!req.finish){
  					req.finish = true;
@@ -45,6 +55,7 @@ http.createServer(function (req, res) {
             return;
         }
     }
+    //不合法的请求直接返回
  	res.writeHead(404,{'Content-Type': 'text/plain','Content-Length':'It\'s not a valid request.'.length});
  	res.end('It\'s not a valid request.');
     console.log(req.url + ' is not a valid request.');
