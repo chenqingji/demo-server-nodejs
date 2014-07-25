6dc9dd02055db34de0ef46ac68c7d017c92142c4
limiao <limiao2005@163.com>
2014-07-25 10:23:21 +0800

    Update login.js
    取消设备号验证

--- src/processors/login.js
+++ src/processors/login.js
@@ -1,6 +1,6 @@
 var qs = require('querystring');
 
-//输入参数email，password，deviceid。该函数用于登录服务器获取授权
+//输入参数email，password。该函数用于登录服务器获取授权
 function process(req,res,db){
     var finish = false;
     var body = '';
@@ -15,12 +15,12 @@ function process(req,res,db){
             console.log("Missing parameter.");
             return;
         }
-        loginUser(postBody.email,postBody.password,postBody.deviceid,db,res,req);
+        loginUser(postBody.email,postBody.password,db,res,req);
     });
 }
 
 //在数据库鉴权，这一步是模拟开发者自己的鉴权行为。通过数据库获取的唯一id和名称注册。
-function loginUser(email,password,deviceId,db,res,req){
+function loginUser(email,password,db,res,req){
     db.get("select * from user where email = ?",email,function(err,row){
         if (err != null){
             res.writeHead(500,{'Content-Type': 'text/plain','Content-Length': "Server error".length});
@@ -32,7 +32,7 @@ function loginUser(email,password,deviceId,db,res,req){
             console.log(email + " is not registered.");
         }else{
             if (password == row.passwd){
-                getToken(row.id,row.username,row.portrait,row.email,deviceId,res,req);
+                getToken(row.id,row.username,row.portrait,row.email,res,req);
             } else {
                 res.writeHead(401,{'Content-Type': 'text/plain','Content-Length': "Password error!".length});
                 res.end("Password error!");
@@ -48,7 +48,7 @@ var http = require('http');
 
 
 //该函数是开发者真正需要在开发中实现的功能。该函数从rong.io换取相应的token用于登录
-function getToken(userId,userName,userPortrait,email,deviceId,res,req) {
+function getToken(userId,userName,userPortrait,email,res,req) {
 	// Build the post string from an object
 	var post_data = qs.stringify({
 		'userId' : userId,
