ea7ad01421566eee015597bc1749d69dae3c5e6f
limiao <limiao2005@163.com>
2014-11-11 01:36:10 +0800

    Update login.js

--- src/processors/login.js
+++ src/processors/login.js
@@ -23,17 +23,21 @@ function process(req,res,db){
 //在数据库鉴权，这一步是模拟开发者自己的鉴权行为。通过数据库获取的唯一id和名称注册。
 function loginUser(email,password,db,res,req){
    // db.get
-   db.query("select * from user where email = ?",email,function(err,row){
+   db.query("select * from user where email = ?",email,function(err,rows){
         if (err != null){
             res.writeHead(500,{'Content-Type': 'text/plain','Content-Length': "Server error".length});
             res.end("Server error");
             console.log(err);
-        }else if (typeof row == "undefined"){
+        }else if (typeof rows == "undefined"){
             res.writeHead(403,{'Content-Type': 'text/plain','Content-Length': "User is not registered.".length});
             res.end("User is not registered.");
             console.log(email + " is not registered.");
         }else{
-            if (password == row.passwd){
+            var row = null;
+            if(rows.length>0) {
+                row = rows[0];
+            }
+            if (row!=null && password == row.passwd){
                 getToken(row.id,row.username,row.portrait,row.email,res,req);
             } else {
                 res.writeHead(401,{'Content-Type': 'text/plain','Content-Length': "Password error!".length});
